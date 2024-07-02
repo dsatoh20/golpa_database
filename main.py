a9d7b50ddb308d7b9cafdb3f9adf25d37e0ed331
dsatoh20 <dsatoh@GQ2DPW2>
2024-07-02 16:24:26 +0900

    cythonで高速化に成功16分>10分

---
+++ main.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from scraper import fetch_and_write
+
+fetch_and_write()
---
+++ setup.py
@@ -0,0 +1,7 @@
+# コンパイル用
+from setuptools import setup
+from Cython.Build import cythonize
+
+setup(
+    ext_modules = cythonize("scraper.pyx")
+)
